5470c4a82767a572bce0b6d24f1dae6982518d6b
Thirasara Dewmin <127840071+thimin@users.noreply.github.com>
2025-09-24 19:18:30 +0530

    comment the origin: process.env.FRONTEND_URL

--- capsule-health-management-system/backend/server.js
+++ capsule-health-management-system/backend/server.js
@@ -18,7 +18,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
